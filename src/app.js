5b77f928b0d87a6c2de13ca30904abdeb206e2e1
Steve Halford <steve@inknpixel.co.uk>
2015-03-11 10:09:09 +0000

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,74 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var ajax = require('ajax');
+
+// Show splash
+var splashCard = new UI.Card({
+    title: " ",
+    body: "Downloading...",
+    icon : "images/djw.png"
+});
+
+splashCard.show();
+
+var parseFeed = function(data, quantity) {
+  var items = [];
+  for(var i = 0; i < quantity; i++) {
+      
+      var item = data[i];
+       
+      // Add to menu items array
+      items.push({
+          title : item.title,
+          subtitle : item.city
+      });
+  }
+ 
+  // Finally return whole array
+  return items;
+};
+
+var URL = 'http://www.designjobswales.co.uk/jobs';
+
+// Download data
+ajax({url: URL, type: 'json'},
+    function(data) {
+        
+        var items = parseFeed( data, 10 );
+        
+        var resultsMenu = new UI.Menu({
+            sections: [{
+                title: 'Design Jobs Wales',
+                items: items
+            }]
+        });
+            
+        resultsMenu.on('select', function(e) {
+            var item = data[e.itemIndex];
+            
+            var detailCard = new UI.Card({
+                title : item.title,
+                subtitle : item.city,
+                body : item.description,
+                scrollable : true,
+                style : "small"
+            });
+            
+            detailCard.show();
+        });
+        
+        resultsMenu.show();
+        splashCard.hide();
+
+    },
+    function(error) {
+        console.log('Ajax failed: ' + error);
+    }
+);
+
+
